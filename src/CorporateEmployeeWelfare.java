7be1be9385d91d0143ffa80078b9114e67d0a78f
Bindu B S <bindubs.216@gmail.com>
2019-09-15 13:01:46 +0530

    #jdbc Connection Commit

--- src/CorporateEmployeeWelfare.java
+++ src/CorporateEmployeeWelfare.java
@@ -1,9 +1,44 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class CorporateEmployeeWelfare {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-		System.out.println("Test1");
+//		System.out.println("Test1");
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "BINDU", "56789");
+			if(conn != null) {
+				System.out.println("connection has established successfully");
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery("SELECT * FROM AdminLogin");
+				System.out.println(" Username\t\tPassword");
+				while(rs.next()) {
+					System.out.print(" " +rs.getString(1));
+					System.out.print("\t\t\t"+rs.getString(2));
+				}
+			}
+			else {
+				System.out.println("NotConnected");
+			}
+		}catch (ClassNotFoundException e) {
+			System.out.println("Driver Class Not Found ! Exeception Occured!");
+		}catch (SQLException se) {
+			System.out.println("Excetion occured while Creating a connection!");
+			System.out.print(se);
+		}finally {
+			try {
+				conn.close();
+			}
+			catch (SQLException sqlE) {
+				System.out.println("Error Occured while Closing Conection!!");
+			}
+		}
 
 	}
 
